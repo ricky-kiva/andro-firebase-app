2ed1f51fc04f6a2fd9d788530dcc17b8dfdfca9e
ricky-kiva <ricky.kiva@gmail.com>
2023-05-07 21:44:03 +0700

    add Messaging feature by DB

--- app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -4,22 +4,31 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FirebaseMessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // get user auth
         auth = Firebase.auth
         val firebaseUser = auth.currentUser
 
@@ -27,6 +36,45 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
             return
+        }
+
+        // initiating Firebase Database
+        db = Firebase.database
+
+        // creates `new reference` to `child node` of root (reference) in location of `MESSAGE_CHILD`
+        val messageRef = db.reference.child(MESSAGES_CHILD)
+
+        // set recyclerview manager
+        val manager = LinearLayoutManager(this)
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+
+        // set options for Firebase Recycler
+        val options = FirebaseRecyclerOptions.Builder<Message>()
+            .setQuery(messageRef, Message::class.java)
+            .build()
+
+        // connect adapter to recycler view
+        adapter = FirebaseMessageAdapter(options, firebaseUser.displayName)
+        binding.messageRecyclerView.adapter = adapter
+
+        // set Message data class on click
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = Message(
+                binding.messageEditText.text.toString(),
+                firebaseUser.displayName.toString(),
+                firebaseUser.photoUrl.toString(),
+                Date().time
+            )
+            // push friendlyMessage to database in `messageRef` location
+            messageRef.push().setValue(friendlyMessage) { error, _ ->
+                if (error != null) {
+                    Toast.makeText(this, getString(R.string.send_error) + error.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.send_success), Toast.LENGTH_SHORT).show()
+                }
+                binding.messageEditText.setText("")
+            }
         }
     }
 
@@ -50,5 +98,21 @@ class MainActivity : AppCompatActivity() {
         auth.signOut()
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    // listen to adapter on resume
+    override fun onResume() {
+        super.onResume()
+        adapter.startListening()
+    }
+
+    // pause listening to adapter while on pause
+    override fun onPause() {
+        adapter.stopListening()
+        super.onPause()
+    }
+
+    companion object {
+        const val MESSAGES_CHILD = "messages"
     }
 }
---
+++ app/src/main/java/com/dicoding/myfirebasechat/Message.kt
@@ -0,0 +1,11 @@
+package com.dicoding.myfirebasechat
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Message (
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val timestamp: Long? = null
+)
